0400f762bffee19ee615399182d89347c156c5fa
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2024-03-18 13:47:06 -0700

    Add script2.js to redfine code. Change output of getComputerChoice() to match playerSelection.

--- script2.js
+++ script2.js
@@ -1,21 +1,27 @@
 "use strict"
 
-// function getComputerChoice() {
-//   const cpu = Math.floor(Math.random() * 3)
-//   if (cpu > 1) {
-//     console.log("Rock);
-//   } else if (cpu < 1) {
-//     console.log("Paper");
-//   } else console.log("Scissors");
-//   console.log(cpu);
-//   return cpu;
-// }
-// getComputerChoice();
 
+function getComputerChoice() {
+  let cpu = Math.floor(Math.random() * 3)
+  if (cpu > 1) {
+    cpu = "Rock";
+    return cpu;
+  } else if (cpu < 1) {
+    cpu = "Paper";
+    return cpu;
+  } else {
+    cpu = "Scissors";
+    return cpu;
+  }
+}
 
 
 
 function playRound(playerSelection, computerSelection) {
+  computerSelection = getComputerChoice();
+  let computerSelectionLow = computerSelection.toLowerCase();
+  console.log(computerSelection);
+
   playerSelection = prompt("Rock, Paper or Scissors?", "Please enter your choice.")
   if (playerSelection === null || playerSelection === "") {
     alert("Game cancelled")
@@ -30,36 +36,33 @@ function playRound(playerSelection, computerSelection) {
     playRound();
   }
 
-  // ComputerSelection: 0 = Rock, 1 = Paper, 2 = Scissors
-  
-  computerSelection = Math.floor(Math.random() * 3)
-  if (computerSelection === 2 && playerSelection === "rock") {
+  if (computerSelectionLow === "scissors" && playerSelection === "rock") {
     console.log("Player chooses Rock, Computer chooses Scissors");
     console.log("Player Wins!");
-  } else if (computerSelection === 1 && playerSelection === "rock") {
+  } else if (computerSelectionLow === "paper" && playerSelection === "rock") {
     console.log("Player chooses Rock, Computer chooses Paper");
     console.log("Player Loses!");
-  } else if (computerSelection === 0 && playerSelection === "paper") {
+  } else if (computerSelectionLow === "rock" && playerSelection === "paper") {
     console.log("Player chooses Paper, Computer chooses Rock");
     console.log("Player Wins!");
-  } else if (computerSelection === 2 && playerSelection === "paper") {
+  } else if (computerSelectionLow === "scissors" && playerSelection === "paper") {
     console.log("Player chooses Paper, Computer chooses Scissors");
     console.log("Player Loses!");
-  } else if (computerSelection === 1 && playerSelection === "scissors") {
+  } else if (computerSelectionLow === "paper" && playerSelection === "scissors") {
     console.log("Player chooses Scissors, Computer chooses Paper");
     console.log("Player Wins!");
-  } else if (computerSelection === 0 && playerSelection === "scissors") {
+  } else if (computerSelectionLow === "rock" && playerSelection === "scissors") {
     console.log("Player chooses Scissors, Computer chooses Rock");
     console.log("Player Loses!");
-  } else if (computerSelection === 0 && playerSelection === "rock" 
-             || computerSelection === 1 && playerSelection === "paper" 
-             || computerSelection === 2 && playerSelection === "scissors"){
-    console.log(`Both players chose ${playerSelection}!`);
+  } else if (computerSelectionLow === playerSelection){
+    console.log(`Both players chose ${computerSelection}!`);
     console.log(("It's a Tie!"));
   }
 
 }
+
 playRound();
+
 
 
 
